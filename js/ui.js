cdb80ff2e222cffe3105352a68ce030fd78ceaeb
boesee <41541999+boesee@users.noreply.github.com>
2025-10-09 14:24:03 +0200

    Replace star characters with FontAwesome icons

--- js/ui.js
+++ js/ui.js
@@ -465,9 +465,9 @@ export class DraftUI {
         const hasHalfStar = (rounded - fullStars) === 0.5 ? 1 : 0;
         const emptyStars = maxStars - fullStars - hasHalfStar;
 
-        const filledStar = '★';
-        const halfStar = '⯨'; // Alternativ: SVG/FontAwesome
-        const emptyStar = '☆';
+        const filledStar = '<i class="fa-solid fa-star" style="color: #FFD700;"></i>';
+        const halfStar   = '<i class="fa-solid fa-star-half-stroke" style="color: #FFD700;"></i>';
+        const emptyStar  = '<i class="fa-regular fa-star" style="color: #FFD700;"></i>';
 
         if (rounded === 0) return this.renderEmptyCell();
         let html = '';
